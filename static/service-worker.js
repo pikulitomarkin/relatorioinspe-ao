ec46e5ec18bca144bc05eca0719cd3c2f86f160b
pikulitomarkin <mpadilha932@gmail.com>
2025-05-31 01:43:10 -0300

    Add files via upload

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,11 +1,14 @@
 const CACHE_NAME = "inspecao-cache-v1";
 const urlsToCache = [
-  "/",
-  "/static/style.css", // Adicione aqui os arquivos CSS
-  "/static/script.js", // Adicione aqui os arquivos JS
-  "/static/manifest.json",
-  "/static/icons/icon-192x192.png",
-  "/static/icons/icon-512x512.png"
+  "/", // Página inicial
+  "/static/manifest.json", // Manifesto do PWA
+  "/static/service-worker.js", // Service Worker
+  "/static/icons/icon-192x192.png", // Ícone
+  "/static/icons/icon-512x512.png", // Ícone
+  "/templates/index.html", // Página HTML
+  "/static/style.css", // Arquivo CSS (se houver)
+  "/static/script.js", // Arquivo JS (se houver)
+  "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js" // Biblioteca para gerar Excel
 ];
 
 // Instala o Service Worker e armazena os arquivos no cache
@@ -22,6 +25,22 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
+    })
+  );
+});
+
+// Atualiza o cache quando uma nova versão do Service Worker é ativada
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
