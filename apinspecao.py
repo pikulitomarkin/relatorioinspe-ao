2cb5080bc5af8c903ab1c201442bcad07a401075
pikulitomarkin <mpadilha932@gmail.com>
2025-06-21 13:35:11 -0300

    Update apinspecao.py

--- apinspecao.py
+++ apinspecao.py
@@ -161,6 +161,6 @@ def load_data_from_file():
 
 if __name__ == '__main__':
     debug_mode = os.getenv('FLASK_DEBUG', 'false').lower() == 'true'
-    app.run(host='0.0.0.0', port=5000, debug=debug_mode)
+    app.run(host='0.0.0.0', port=5005, debug=debug_mode)
 
 []
